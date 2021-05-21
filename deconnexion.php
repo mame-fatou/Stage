8b2a014122608d9cfaec641e9a3068f1a726315b
mame-fatou <ndeyelebayame@gmail.com>
2021-05-21 18:56:03 +0200

    page deconnexion

---
+++ deconnexion.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+session_destroy();
+
+?>
+<!DOCTYPE HTML>
+
+
+<HTML>
+<HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<TITLE>page de deconnexion</TITLE>
+
+
+</HEAD>
+<body>
+	
+
+		<img width=400px src ="images/logo.png" alt="logo"> 
+
+ <H1>A bientot !</H1>
+ 
+
+
+ 
+</body>
+</html>
