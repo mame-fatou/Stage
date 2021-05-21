b29ba7b63fa79b4319982e1628cba9bf118be9a4
mame-fatou <ndeyelebayame@gmail.com>
2021-05-21 18:22:28 +0200

    page d'accueuil
    git config --global user.name 'mame-fatou'

---
+++ accueil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+
+  <title>ACCUEIL</title>
+
+</head>
+	<body>
+		
+
+
+
+</body>
+</html>
